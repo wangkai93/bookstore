7703e0925528bc998fe491fcbf757b8fce454e36
carter <carterwang@izmkh.com>
2017-03-18 14:11:08 +0800

    add generic

--- src/main/java/cn/edu/zzia/bookstore/dao/impl/BookDaoImpl.java
+++ src/main/java/cn/edu/zzia/bookstore/dao/impl/BookDaoImpl.java
@@ -14,16 +14,15 @@ import cn.edu.zzia.bookstore.domain.Book;
 @Repository(IBookDao.SERVICE_NAME)
 public class BookDaoImpl extends CommonDaoImpl<Book> implements IBookDao {
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public Integer getTotalRecord() {
 
 		final String hql = "select count(*) from Book";
 
-		return this.getHibernateTemplate().execute(new HibernateCallback() {
+		return this.getHibernateTemplate().execute(new HibernateCallback<Integer>() {
 
 			@Override
-			public Object doInHibernate(Session session) throws HibernateException, SQLException {
+			public Integer doInHibernate(Session session) throws HibernateException, SQLException {
 
 				Query query = session.createQuery(hql);
 				return ((Number)query.uniqueResult()).intValue();
